a3908b129db7581628ceefa4d8325dce87040f9d
Daniel <daniel9805@hotmail.com>
2023-06-05 17:18:03 -0500

    minor changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-<<<<<<< Updated upstream
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-=======
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./context/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
->>>>>>> Stashed changes
   ],
   darkMode: "class",
   theme: {
     extend: {
-<<<<<<< Updated upstream
-=======
       animation: {
         "zoom-in": "zoom-in .7s ease-in",
         "slide-in": "slide-in 1s ease-in-out",
@@ -44,13 +36,12 @@ module.exports = {
           },
         },
       },
->>>>>>> Stashed changes
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
+};
